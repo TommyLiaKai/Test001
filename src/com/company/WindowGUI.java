b346d1ad7815341ed9bb7058497c8581d594221f
Tommy <tommyliakai@gmail.com>
2021-08-27 19:34:00 +0200

    added file path to images

--- src/com/company/WindowGUI.java
+++ src/com/company/WindowGUI.java
@@ -6,24 +6,23 @@ import java.awt.event.MouseEvent;
 
 public class WindowGUI {
 
-    public static void showImage(String picture) {
-        ImageIcon image = new ImageIcon(picture);
+    public static void showImage(String image) {
         JLabel label = new JLabel();
-        label.setIcon(image);
-        createWindow(label);
+        label.setIcon(new ImageIcon("C:/Users/tommy/IdeaProjects/Test01/images/" + image));
+        new WindowGUI().createWindow(label);
     }
 
-    public static void createWindow(JLabel label) {
+    private void createWindow(JLabel label) {
         JFrame frame = new JFrame("Ships");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1210,630);
         frame.setVisible(true);
         frame.add(label);
-        closeWindow(frame);
+        new WindowGUI().closeWindow(frame);
     }
 
-    public static void closeWindow(JFrame frame) {
+    private void closeWindow(JFrame frame) {
         frame.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
